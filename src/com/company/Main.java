f0026b80a2b14d569dd255894bfebb3424a802a4
Nikhil Pratap Singh <pratapnikhil106@gmail.com>
2022-03-18 22:53:52 +0530

    java tutorial

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String [] args ){
+        int number1 = 34;
+        int number2 = 87;
+        int number3 = 987;
+        int sum = number1+number2+number3;
+        System.out.print("The sum of the 3 number are as: ");
+        System.out.println(sum);
+    }
+}
