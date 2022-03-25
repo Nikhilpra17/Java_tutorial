3f9a05ad5bfd8de8af16ddc99342a1a701df2c7b
Nikhilpra17 <97670140+Nikhilpra17@users.noreply.github.com>
2022-03-25 22:07:05 +0530

    float concate

---
+++ pro2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class pro2 {
+    public static void main(String[] args) {
+        float aa= 1.72f;
+        float aw= 2.56f;
+        float mutti= (aa*aw);
+        System.out.println(mutti);
+    }
+}
