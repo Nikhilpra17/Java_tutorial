66ef1f004339136256c1bc1c8e2901a02da59be2
Nikhilpra17 <97670140+Nikhilpra17@users.noreply.github.com>
2022-03-25 22:07:40 +0530

    modulus tuto

---
+++ pro3.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class pro3 {
+    public static void main(String[] args) {
+        int divisor= 5;
+        int divident= 49;
+        int remainder= divident%divisor;
+        System.out.println("When the number "+ divident+ " is divided by "+divisor +" , then the " +
+                "remainder comes to be "+ remainder);
+    }
+}
