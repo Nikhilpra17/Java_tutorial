6b92cde459e7efffe3e526efa1204965c3b8558d
Nikhilpra17 <97670140+Nikhilpra17@users.noreply.github.com>
2022-03-27 23:53:38 +0530

    detention

---
+++ definingfxn.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class definingfxn {
+    public static void main(String[] args) {
+        detention();
+        detention();
+        detention();
+        detention();
+        detention();
+        detention();
+    }
+        //what Bart did so far
+        public static void detention() {
+            System.out.println("I will not copy and paste code.");
+            System.out.println("I will use Camel Case when writing class names.");
+            System.out.println("I will use lower Camel Case when writing function names.");
+            System.out.println("I will use lower Camel Case when writing variables names.\n");
+        }
+
+        // Task 2 – call the function 6 times.
+}
+
+
+
+
+        //Task 1 - Make a function here. See the doc comment for details.
+
+        /*
+         * Funtion name: printLines
+         *
+         * Inside the function:
+         *   1. prints the four lines
+         */
+
+
+
