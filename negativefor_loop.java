2c4fb696399656e3c50d726e40b84e9c2c569da9
Nikhilpra17 <97670140+Nikhilpra17@users.noreply.github.com>
2022-03-29 23:49:27 +0530

    Beer song

---
+++ negativefor_loop.java
@@ -0,0 +1,11 @@
+package com.company;
+//Beer Song
+
+public class negativefor_loop {
+    public static void main(String[] args) {
+        // Task 2: make a for loop that starts at i = 99, and finishes at i=1.
+    for (int i =99; i>0; i --){
+        System.out.println( i+ " bottles of beer on the wall," + i + " bottles of beer!\n take one down, pass it around, "+(i-1) + " bottles of beer on the wall!\n");
+        }
+    }
+}
