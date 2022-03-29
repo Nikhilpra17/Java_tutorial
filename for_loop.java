c9dd5f2cf3e3dcc28adfd1fbc4406c650cec76dd
Nikhilpra17 <97670140+Nikhilpra17@users.noreply.github.com>
2022-03-29 23:48:54 +0530

    for loop example

---
+++ for_loop.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.math.*;
+
+public class for_loop {
+
+
+    public class rough2 {
+        public static void main(String[] args) {
+//        1st Problem
+        /*
+        for (int i =1; i <=10; i++) {
+            System.out.println(i+"." + " If Java was easy, they would call it Python!");
+        */
+
+
+//        2nd Problem
+        /*
+        Scanner scan= new Scanner(System.in);
+        System.out.println(" Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+        String input = scan.nextLine();
+
+        for (int i =1; i<=99; i ++){
+            System.out.println(i+ "- " + input);
+        }
+
+        scan.close();
+        */
+
+//        Problem 3
+        /*
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Hi Timmy! Choose a number to count to:");
+        int num = scan1.nextInt();
+
+        for (int i = 0; i<= num; i++ ){
+            System.out.print(i + ", ");
+        }
+
+        scan1.close();
+        */
+        }
+
+    }
+
+}
